dda49d019834f090d0a0d654148c59812a1f1116
AbdullahRFA <shakibrybmn@gmail.com>
2025-04-10 20:28:33 +0600

    added form for user registration and login and change in Customer user model retailer with dealer

--- RSCMS_app/models.py
+++ RSCMS_app/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
         ('admin', 'Admin'),
         ('manager', 'Manager'),
         ('customer', 'Customer'),
-        ('retailer', 'Retailer'),
+        ('dealer', 'Dealer'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='admin')
     
