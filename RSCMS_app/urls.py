e568322b017266c653b31799e632d5091adf339c
AbdullahRFA <shakibrybmn@gmail.com>
2025-04-10 22:56:17 +0600

    added login functionality

--- RSCMS_app/urls.py
+++ RSCMS_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from RSCMS_app import views
 
 urlpatterns = [
-    path('register/',views.register_view, name='register')
+    path('register/',views.register_view, name='register'),
+    path('login/',views.login_view, name='login'),
 ]
--- RSCMS_app/views.py
+++ RSCMS_app/views.py
@@ -14,7 +14,24 @@ def register_view(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse("Registration Successfull")
+            return redirect('login_view')
     else:
         form = CustomUserCreationForm()
     return render(request,'auth/register.html',{'form':form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return HttpResponse("Login Successfully")
+            # return role_based_redirect(user)
+        else:
+            messages.error(request,"Invalid username of password")
+    else:
+        form = AuthenticationForm()
+    return render(request,'auth/login.html',{'form':form})
+
+def role_based_redirect(user):
+    pass
