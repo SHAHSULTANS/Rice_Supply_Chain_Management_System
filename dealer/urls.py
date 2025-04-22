dbc92b8175ceb55a205ea42461cb30e9b22ed707
Shah Sultan <sultan91234567@gmail.com>
2025-04-22 11:44:46 +0600

    Add edit button so that dealer can modify the post

--- dealer/urls.py
+++ dealer/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('dashboard/', views.dealer_dashboard, name='dealer_dashboard'),
     path('dealer-profile/<int:user_id>/', views.dealer_profile_create, name='dealer_profile_create'),
     path('add-post/', views.add_paddy_post, name='add_paddy_post'),
-    path('marketplace/all_post', views.marketplace_paddy_posts, name='marketplace_paddy_posts'),
-
-
+    path('marketplace/all_post', views.see_all_paddy_posts, name='marketplace_paddy_posts'),
+    path('paddy/edit/<int:post_id>/', views.edit_paddy_post, name='edit_paddy_post'),
 ]
