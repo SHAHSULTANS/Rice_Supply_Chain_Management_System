687885126fec313b47927c60ddf1f74aa6fdac66
khaled1913 <khaledmahmud190192@gmail.com>
2025-04-23 10:41:58 +0600

    added admin_profile functionality

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/admin/',views.admin_dashboard, name='admin_dashboard'),
+    path('admin_profile/',views.admin_profile, name='admin_profile'),
+    path('update_admin_profile/',views.update_admin_profile, name='update_admin_profile'),
 
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
-
+from .forms import AdminProfileForm
+from .models import AdminProfile
 
 # Create your views here.
 
@@ -11,3 +12,10 @@ def check_admin(user):
 @user_passes_test(check_admin)
 def admin_dashboard(request):
     return render(request, 'admin/dashboard.html',{'role':'admin'})
+
+def update_admin_profile(request):
+    return render(request,"admin/update_admin_profile.html")
+
+def admin_profile(request):
+    admin, created = AdminProfile.objects.get_or_create(user=request.user)
+    return render(request,"admin/admin_profile.html",{'admin':admin})
