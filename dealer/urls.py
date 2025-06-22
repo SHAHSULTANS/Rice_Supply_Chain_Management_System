7456fcfbac3f7ed5c927bcb0d1487898735cd460
Shah Sultan <sultan91234567@gmail.com>
2025-06-22 19:08:41 +0600

    add paddy detail views, templates & urls when     before order by manager, he can see details about it.

--- dealer/urls.py
+++ dealer/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('dealer-profile/<int:user_id>/', views.dealer_profile_create, name='dealer_profile_create'),
     path('add-post/', views.add_paddy_post, name='add_paddy_post'),
     path('marketplace/all_post', views.see_all_paddy_posts, name='marketplace_paddy_posts'),
+    path('paddy/<int:post_id>/', views.paddy_detail, name='paddy_detail'),
+    
     path('paddy/edit/<int:post_id>/', views.edit_paddy_post, name='edit_paddy_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
     path('profile/edit/',views.edit_dealer_profile, name='edit_dealer_profile'),
